e549186cba901e376f647a0bb821a3a0075a6a50
simaoqingnian <simaoqingnian@163.com>
2016-07-03 21:04:29 +0800

    helpActivity add back button

--- src/com/puzzleworld/onecolor/HelpActivity.java
+++ src/com/puzzleworld/onecolor/HelpActivity.java
@@ -2,6 +2,9 @@ package com.puzzleworld.onecolor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 /*
  * 作品展示页面
@@ -9,10 +12,20 @@ import android.os.Bundle;
 
 public class HelpActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_help);
+    private ImageView ivBack;
 
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        ivBack = (ImageView) findViewById(R.id.ivBack);
+
+        ivBack.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                HelpActivity.this.finish();
+            }
+        });
+    }
 }
